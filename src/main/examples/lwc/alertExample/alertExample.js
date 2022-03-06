d904a89246b5e6fc96c809d383863dfd690bdf9c
kratoon3 <ondrej@kratochvil.dev>
2022-03-06 13:41:26 +0100

    Remove examples

--- src/main/examples/lwc/alertExample/alertExample.js
+++
@@ -1,9 +0,0 @@
-import {LightningElement, track} from 'lwc';
-
-export default class AlertExample extends LightningElement {
-    @track isClosed = false;
-
-    handleCloseClick() {
-        this.isClosed = true;
-    }
-}
