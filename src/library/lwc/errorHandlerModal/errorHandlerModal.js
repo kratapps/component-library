8593cbbb813c6f299a059b2c6c44d70e7847bf6b
kratoon <ondrej@kratochvil.dev>
2024-04-02 12:24:44 +0200

    Improve LightningError.cls serialization. Add LightningResponse.cls.

--- src/library/lwc/errorHandlerModal/errorHandlerModal.js
+++ src/library/lwc/errorHandlerModal/errorHandlerModal.js
@@ -1,57 +1,35 @@
 /*
  * BSD 3-Clause License
  *
- * Copyright (c) 2024, kratapps.com
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice, this
- *    list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- *
- * 3. Neither the name of the copyright holder nor the names of its
- *    contributors may be used to endorse or promote products derived from
- *    this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * Copyright (c) 2022, kratapps.com
+ * See the LICENSE: https://github.com/kratapps/component-library/blob/main/LICENSE
  */
 
 /**
- * @file Modal for Error Handler.
+ * @file    Modal for Error Handler.
+ *
+ * @link    https://docs.kratapps.com/component-library/error-handler
  *
  * @author  kratapps.com
- * @see     https://docs.kratapps.com/component-library/error-handler
  */
 import { api } from 'lwc';
 
 import LightningModal from 'lightning/modal';
 
-/**
- * @type {ErrorHandlerAction[]}
- */
-export const standardActions = [{ label: 'Close', variant: 'brand', name: 'close' }];
+export const standardActions = [{ label: 'Close', variant: 'neutral', name: 'close' }];
 
 export default class ErrorHandlerModal extends LightningModal {
-    @api label = 'Something went wrong.';
+    @api label;
     @api message;
+    @api fieldErrors;
     @api actions;
 
     get allActions() {
         return this.actions ?? standardActions;
+    }
+
+    get allFieldErrors() {
+        return this.fieldErrors;
     }
 
     handleActionClick(event) {
